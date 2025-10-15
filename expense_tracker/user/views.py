e065d1efe2ad7bf2db67c4e153e53e7e2e75f8a6
Prabin Karki <161217370+Prabinkarki67@users.noreply.github.com>
2025-10-15 08:51:49 +0545

    done with piechart render and views

--- expense_tracker/user/views.py
+++ expense_tracker/user/views.py
@@ -3,16 +3,46 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Expense
+from django.db.models import Sum 
+
+
+import matplotlib
+import matplotlib.pyplot as plt
+import io, base64
+
+matplotlib.use('Agg')  # Prevent GUI errors
 
 
 
-    
+
 @login_required
 def user_page(request):
     expenses = Expense.objects.filter(user=request.user)
+    total = expenses.aggregate(Sum('amount'))['amount__sum'] or 0
+    
+    category_data = (
+        expenses.values('category')
+        .annotate(total=Sum('amount'))
+        .order_by('category')
+    )
+    categories = [item['category'] for item in category_data]
+    category_totals = [float(item['total']) for item in category_data]    
+    fig, ax = plt.subplots()
+    ax.pie(category_totals, labels = categories, autopct = '%1.1f%%', startangle = 90)
+    ax.axis('equal')
+    
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(image_png).decode('utf-8')    
+    
     return render(request, 'user/user.html', {
         'user':request.user,
-        'expenses': expenses})
+        'expenses': expenses, 
+        'total': total,
+        'graphic': graphic})
 
 
 def login_user(request):
